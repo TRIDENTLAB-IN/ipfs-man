0dea3b420bd81305cc5124be024b91ba961c869b
Aaryadev <devmgbhat@gmail.com>
2025-05-21 15:02:09 +0530

    basic api calls

--- app/Controllers/Api.php
+++ app/Controllers/Api.php
@@ -1,13 +1,58 @@
 <?php
 /*
-this api will allow you to  
+this api will allow you to
 */
 namespace App\Controllers;
 
 class Api extends BaseController
 {
-    public function index(): string
-    {
+  public function index(): string
+  {
+    return "aarya.dev";
+  }
+  public function id(){
 
+    return $this->ipfsapi("id",null);
+  }
+  public function bw(){
+
+    return $this->ipfsapi("stats/bw",null);
+  }
+
+
+  public function stat(){
+
+    return $this->ipfsapi("repo/stat",null);
+  }
+
+
+  private function ipfsapi($url,$post_data){
+    $apiurl =  env("IPFS_API").$url;
+
+    $ch=curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $apiurl);
+
+    // post_data
+    curl_setopt($ch, CURLOPT_POST, true);
+    if(!is_null($post_data)){
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
     }
+
+
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
+
+    curl_setopt($ch, CURLOPT_VERBOSE, true);
+
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return $response;
+
+
+  }
 }
